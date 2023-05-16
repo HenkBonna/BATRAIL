a5b23af7ac5a0d927ab5e0ced84b544539411bae
HenkBonna <henrik.b.stangeland@gmail.com>
2023-05-16 18:32:28 +0200

    Fixed stuff, and removed garbage :)

--- python/mainv2.py
+++ python/mainv2.py
@@ -4,64 +4,40 @@ import numpy as np
 import scipy as sp
 import matplotlib.gridspec as gridspec
 
-
-
 ###########################################################
 ####  PLOT STUFF ####################################### 
 
-gs = gridspec.GridSpec(nrows=3, ncols=2, width_ratios=[1, 1.5], height_ratios=[1, 1, 1.5])
+gs = gridspec.GridSpec(nrows=3, ncols=2, width_ratios=[1, 1.5], height_ratios=[1, 1, 1.25])
 
 # Create the three axes
-fig = plt.figure(constrained_layout=True, figsize=(12, 8))
-
-# Upper GridSpec
-gs0 = gridspec.GridSpec(2,1, height_ratios=[2, 1], figure=fig)
-
-# Top Gridspec; Graphs
-gs00 = gridspec.GridSpecFromSubplotSpec(ncols=2, nrows=3, width_ratios=[1, 3], height_ratios=[1, 1, 1], subplot_spec=gs0[0])
-
-# Bottom Gridspec: Info
-gs01 = gridspec.GridSpecFromSubplotSpec(ncols=4, nrows=1, subplot_spec=gs0[1])
+fig = plt.figure(constrained_layout=True, figsize=(12, 7))
 
 # Define Graph axes
-ax0 = fig.add_subplot(gs00[0, 0]) 
-ax1 = fig.add_subplot(gs00[1, 0]) 
-ax2 = fig.add_subplot(gs00[2, 0]) 
-ax3 = fig.add_subplot(gs00[:, 1])
+ax_temp = fig.add_subplot(gs[0, 0]) 
+ax_charge = fig.add_subplot(gs[1, 0]) 
+ax_info_1 = fig.add_subplot(gs[2, 0]) 
+ax_info_2 = fig.add_subplot(gs[2, 1]) 
+ax_main = fig.add_subplot(gs[0:2, 1])
 
-# Define legend axes
-
-textstr = "to"
-
-ax_mach_info = fig.add_subplot(gs01[0,0])
-ax_batt_info = fig.add_subplot(gs01[0,1])
-ax_envi_info = fig.add_subplot(gs01[0,2])
-ax_asso_info = fig.add_subplot(gs01[0,3])
-
-ax_mach_info.axis('off')
-ax_batt_info.axis('off')
-ax_envi_info.axis('off')
-ax_asso_info.axis('off')
-
+ax_info_1.axis('off')
+ax_info_2.axis('off')
 props = dict(boxstyle='square', facecolor='white', alpha=0.5)
 
-#fig, ((ax0, ax1), (ax2, ax3)) = plt.subplots(2, 2)
-
 ###########################################################
-####  FORMULA STUFF ####################################### 
+####  MODELLING STUFF ####################################### 
 
 class Environment():
-    def __init__(self, name, altitude, temperature):
+    def __init__(self, name, altitude, temperature_lists):
         self.name = name
         self.altitude = altitude
-        self.temperature = temperature
+        self.temperature_lists = temperature_lists
 
 class Machine():
-    def __init__(self, name, wattage, weight, Battery):
+    def __init__(self, name, weight, battery, consumption):
         self.name = name
-        self.wattage = wattage
         self.weight = weight
-        self.Battery = Battery
+        self.battery = battery
+        self.consumption = consumption
         
 class Battery():
     def __init__(self, name, capacity, charge_rate, weight):
@@ -74,219 +50,200 @@ class Battery():
 # Nested temperature control should affect the battery
 # Perhaps model the box around the battery
 
-def simulate(duration, Machine, Battery, Environment):
-    m = Machine
-    b = Battery
+def simulato(duration, List_Of_Machines, Environment):
     e = Environment
+    time_list = np.arange(0, duration, 1, dtype=int)
+    temp_profiles = []
+    for temps in e.temperature_lists:
+        temp_profile = expand_array(np.array(temps), duration)
+        temp_profiles.append(temp_profile)
 
-    w1 = Machine.wattage    # two consumptions
-    w2 = Machine.wattage    #
+    #temp_list = temp
+    charge_lists = []
 
-    t = 0
-    temp = Environment.temperature
-    alt = Environment.altitude
+    list_of_results = []
+    for i in range(len(List_Of_Machines)):
+        if len(List_Of_Machines)!=len(temp_profiles): # Ensure conforming, otherwise only 1 temp profile is used
+            temp_list = temp_profiles[0]
+        else:
+            temp_list = temp_profiles[i]
+        b = List_Of_Machines[i].battery
+        capacity = b.capacity 
+        result = []
+        # Running the simulation
+        t = 0
+        charging = False
+        temps = []
+        charge_list = []
+        while t<duration:
+            result.append(capacity)
+            chrg = 0
+            if capacity < 0.2*b.capacity: 
+                charging = True
+            if charging == True:
+                chrg = chargo(10, temp_list[t])
+                capacity = capacity + chrg # TODO: Fix this
+                if capacity > 0.9*b.capacity:
+                    charging = False
+            else:
+                capacity = capacity - depleto((List_Of_Machines[i].consumption/60),temp_list[t]) # TODO: Fix this
 
-    time_list, wattage_list_1, wattage_list_2, temp_list, altitude_list, charge_list = [], [], [], [], [], []
+            if(capacity<0):
+                capacity=0
+            elif(capacity>1000):
+                capacity=1000
 
-    usage_hi = 300/60 #TODO: add these into the Machine object. It's hourly, so /60
-    usage_lo = 100/60
 
-    charge=0
-    # Running the simulation
-    while t<duration:
-
-        temp = temp + np.random.randint(-1,2)
-        alt = alt + np.random.randint(-1,2)
-
-        time_list.append(t)
-        wattage_list_1.append(w1)
-        wattage_list_2.append(w2)
-        temp_list.append(temp)
-        altitude_list.append(alt)
-        charge_list.append(charge)
-        t = t+1
-
-        # Making some random temperature & height variations; replace with data
-        # TODO: Can also plot altitude and temperatures
-        
-        temp_coeff = np.abs(1 - temp / 20)
-        w1 = w1 - usage_hi * temp_coeff
-        #w1 = w1 - battery_depletion(w1,temp,1,0.1)
-        
-        if(w1<0):
-            w1=0
-        elif(w1>1000):
-            w1=1000
-
-        w2 = w2 - usage_lo * temp_coeff
-        #w2 = w2 - battery_depletion(w2,temp,1,0.1)
-        
-        if(w2<0):
-            w2=0
-        elif(w2>1000):
-            w2=1000
-
+            charge_list.append(chrg)
+            t = t+1
+        list_of_results.append(result)
+        charge_lists.append(charge_list)
 
     # Create the plot
-    
 
     # Subgraphs
-    ax0.plot(time_list,temp_list, 'tab:orange')
-    ax0.set(xlabel='Time [min]', ylabel='Temperature [C]')
-    ax1.plot(time_list,altitude_list, 'tab:green')
-    ax1.set(xlabel='Time [min]', ylabel='Altitude [m]')
-    ax2.plot(time_list,charge_list, 'tab:red')
-    ax2.set(xlabel='Time [min]', ylabel='Charging rate')
+    ax_temp.set(ylabel='Temperature [C]')
+    #ax_charge.plot(time_list,charge_list, 'tab:green')
+    ax_charge.set(xlabel='Time [min]', ylabel='Chargin rate')
 
     # Main Graph
-    ax3.plot(time_list,wattage_list_1)
-    ax3.plot(time_list,wattage_list_2, 'tab:red')
-    ax3.set(xlabel='Time [m]', ylabel='Capacity [kWh]')
 
-    # Info Section
-    mach_str = m.name
-    batt_str = b.name
-    envi_str = e.name
-    asso_str = "INFO"
+    info_str_left = ""
+    info_str_right = ""
 
-    ax_mach_info.text(
-        0.05, 0.95, mach_str, transform=ax_mach_info.transAxes, fontsize=14, va='top', ha='right', bbox=props)
-    ax_batt_info.text(
-        0.05, 0.95, batt_str, transform=ax_batt_info.transAxes, fontsize=14, va='top', ha='center', bbox=props)
-    ax_envi_info.text(
-        0.05, 0.95, envi_str, transform=ax_envi_info.transAxes, fontsize=14, va='top', ha='center', bbox=props)
-    ax_asso_info.text(
-        0.05, 0.95, asso_str, transform=ax_asso_info.transAxes, fontsize=14, va='top', ha='center', bbox=props)
+    #info_str_left += "Machine:    " + List_Of_Machines[0].name + "\n     Consumption (Low):     " + str(List_Of_Machines[0].consumption) + "\nBattery:    " + b.name + "\n     Capacity:    " + str(b.capacity) + "\nStretch:    " + e. name + "\n";
 
+    col='tab:green'
+    for i in range(len(list_of_results)):
+        col = cycle_col(col)
+        # Plot all temps
+        if len(List_Of_Machines)!=len(temp_profiles): # Ensure conforming, otherwise only 1 temp profile is used
+            ax_temp.plot(time_list,temp_profiles[0], col)
+        else:
+            ax_temp.plot(time_list,temp_profiles[i], col)
+        # Plot all depletions
+        ax_main.plot(time_list,list_of_results[i], col) # TODO: The m and b values are after assigning
+        ax_charge.plot(time_list, charge_lists[i], col)
+    ax_main.set(xlabel='Time [min]', ylabel='Capacity [kWh]')
 
+    info_str_left += "Stretch:        " + str(e.name) + "\n" + "Machine:        " + str(List_Of_Machines[0].name) + "\n" + "Battery:        " + str(List_Of_Machines[0].battery.name) + " – " + str(List_Of_Machines[0].battery.capacity) + " kWh\n" + "Consumption (Low):    " + str(List_Of_Machines[0].consumption) + " kWh/h\n" + "Consumption (High):    " + str(List_Of_Machines[1].consumption) + " kWh/h\n"
 
-# CHAT GPT
-def battery_depletion(capacity, temperature, time, C_rate):
-    """
-    Simulates battery depletion over time, given a certain capacity, temperature, discharge rate, and time.
+    try:
+        info_str_right += "Stretch:        " + str(e.name) + "\n" + "Machine:        " + str(List_Of_Machines[0].name) + "\n" + "Battery:        " + str(List_Of_Machines[2].battery.name) + " – " + str(List_Of_Machines[2].battery.capacity) + " kWh\n" + "Consumption (Low):    " + str(List_Of_Machines[2].consumption) + "\n" + "Consumption (High):    " + str(List_Of_Machines[3].consumption) + "\n"
+    except:
+        info_str_right = ""
 
-    Parameters:
-    -----------
-    capacity : float
-        Battery capacity in watt-hours (Wh)
-    temperature : float
-        Operational temperature of the battery in degrees Celsius (°C)
-    time : array_like
-        Discharge time in hours (h)
-    C_rate : float
-        Discharge rate, expressed as a multiple of the capacity (C)
+    ax_info_1.text(0, 0, info_str_left, fontsize=12, bbox=props)
+    ax_info_2.text(0, 0, info_str_right, fontsize=12, bbox=props)
 
-    Returns:
-    --------
-    discharge_capacity : array_like
-        Discharge capacity over time in ampere-hours (Ah)
-    """
-    # Convert capacity from Wh to Ah
-    voltage = 3.7  # Typical voltage for an NMC battery
-    capacity_Ah = capacity / voltage
+# A very rough way of caluclating depletion-variation with temperature.
+def depleto(consumption, temperature):
+    temp_coeff = 1.00
+    if temperature < -35:
+        temp_coeff = 4.00
+    elif temperature > -35 and temperature <= -25:
+        temp_coeff = 2.00 
+    elif temperature > -25 and temperature <= -15:
+        temp_coeff = 1.50
+    elif temperature > -15 and temperature <= -5:
+        temp_coeff = 1.25
+    elif temperature > -5 and temperature <= +5:
+        temp_coeff = 1.125
+    elif temperature > -15 and temperature <= -10:
+        temp_coeff = 1.05
 
-    # Calculate rate constant k based on temperature
-    k_ref = 2.0e-5  # Reference rate constant at 25°C
-    E_r = 34500  # Activation energy
-    T_ref = 298.15  # Reference temperature in Kelvin
-    R = 8.314  # Gas constant
-    k = k_ref * np.exp(-E_r / (R * (temperature + 273.15)) + E_r / (R * T_ref))
+    temp_coeff += abs(temperature-np.round(temperature, 0))
 
-    # Calculate discharge capacity over time
-    discharge_capacity = capacity_Ah * (1 - np.exp(-k * C_rate * time))
+    res = temp_coeff*consumption
+    #print(abs(temperature-np.round(temperature, 0)))
+    return res
 
-    return discharge_capacity
+def chargo(charge_rate, temperature):
+    temp_coeff = 1.00
+    if temperature < -35:
+        temp_coeff = 0.20
+    elif temperature > -35 and temperature <= -25:
+        temp_coeff = 0.50 
+    elif temperature > -25 and temperature <= -15:
+        temp_coeff = 0.75
+    elif temperature > -15 and temperature <= -5:
+        temp_coeff = 0.85
+    elif temperature > -5 and temperature <= +5:
+        temp_coeff = 0.90
 
+    temp_coeff += abs(temperature-np.round(temperature, 0))/100
 
-## CHAT-GPT v2
-def discharge_capacity(temp, capacity, time):
-    # Define the temperature-dependent parameters
-    if temp <= 0:
-        # Handle the case where the temperature is below 0 degrees Celsius
-        C_rate = 0.1
-        k = 4.0
-    elif temp <= 25:
-        # Handle the case where the temperature is between 0 and 25 degrees Celsius
-        C_rate = 1.0
-        k = 1.0
-    elif temp <= 40:
-        # Handle the case where the temperature is between 25 and 40 degrees Celsius
-        C_rate = 2.0
-        k = 0.5
-    else:
-        # Handle the case where the temperature is above 40 degrees Celsius
-        C_rate = 0.5
-        k = 2.0
-    
-    # Calculate the discharge capacity
-    discharge_capacity = capacity * np.exp(-k * C_rate * time)
-    
-    print(discharge_capacity)
-    return discharge_capacity
+    res = temp_coeff*charge_rate
+    #print(res)
+    return res
 
+def cycle_col(col):
+    res = col
+    if col == 'tab:green':
+        res = 'tab:blue'
+    elif col == 'tab:blue':
+        res = 'tab:cyan'
+    elif col == 'tab:cyan':
+        res = 'tab:red'
+    elif col == 'tab:red':
+        res = 'tab:orange'
+    elif col == 'tab:orange':
+        res = 'tab:olive'
+    elif col == 'tab:olive':
+        res = 'tab:green'
+    return res
 
-def battery_depletion1(E0, k, t, T, Ea, A):
-    """
-    Calculates the remaining energy in a battery as a function of time and temperature,
-    assuming an exponential decay model with temperature dependence.
+## Function modified from one generated by ChatGPT
+def expand_array(input_array, length):
+    # Get the number of points in the input array
+    n = input_array.shape[0]
+    # Create an array of indices corresponding to the input points
+    indices = np.arange(n)
+    # Create an array of indices corresponding to the output points
+    new_indices = np.linspace(0, n - 1, length)
+    # Interpolate the input data at the output points
+    output_array = np.interp(new_indices, indices, input_array)
 
-    Parameters:
-    E0 (float): the initial energy of the battery
-    k (float): the decay constant, in units of inverse time
-    t (numpy array): an array of time values at which to evaluate the energy
-    T (float or numpy array): the temperature of the battery, in Kelvin
-    Ea (float): the activation energy, in units of energy per mole
-    A (float): the pre-exponential factor, in units of inverse time
+    return output_array
 
-    Returns:
-    E (numpy array): an array of energy values corresponding to the input time and temperature arrays
-    """
-    R = 8.314  # gas constant, in units of J/mol/K
+def scenario_1():
+    # DIFFERENT BATTERY TYPES, WITH VARYING CONSUMPTIONS
+    battery_1 = Battery('NMC',800, 0.8, 6800)
+    battery_2 = Battery('LTO',600, 0.8, 7260)
+    machine_1 = Machine('LT15', 7000, battery_1, 300)
+    machine_2 = Machine('LT15', 7000, battery_1, 100)
+    machine_3 = Machine('LT15', 7000, battery_2, 300)
+    machine_4 = Machine('LT15', 7000, battery_2, 100)
 
-    #
-    # In this version of the function, we have added three new parameters:
+    temps_1 = [-19.20, -18.20, -22.01, -19, -20, -21, -20.29, -19.20, -18.10]
+    temps_2 = [-15, -14, -12, -16]
 
-    #T: the temperature of the battery, in Kelvin
-    #Ea: the activation energy, which describes the dependence of the decay constant on temperature
-    #A: the pre-exponential factor, which is another parameter that describes the temperature dependence of the decay constant.
-    #The formula for the remaining energy now includes an additional term that depends on temperature, which is given by A * exp(-Ea/(R*T)), #where R is the gas constant in units of J/mol/K. This term describes the temperature dependence of the decay constant k, and accounts for #the fact that batteries tend to lose energy more quickly at higher temperatures due to increased chemical and electrochemical reactions.
+    environment = Environment('Skøyen - Spikkestad', 110, [temps_1, temps_1])
+    duration = 180
 
-    #Note that the activation energy Ea and pre-exponential factor A are specific to each battery chemistry and geometry, and can be estimated #from experimental data or literature values.
+    simulato(duration, [machine_1, machine_2, machine_3, machine_4], environment)
+    plt.grid()
+    plt.show()
+    plt.title("Different Consumptions")
 
-    E = E0 * np.exp(-A * np.exp(-Ea/(R*T)) * t - k * t)
-    return E
+def scenario_2():
+    # Different Temperatures
+    battery_1 = Battery('NMC',800, 0.8, 6800)
+    battery_2 = Battery('LTO',600, 0.8, 7260)
+    machine_1 = Machine('LT15', 7000, battery_1, 300)
+    machine_2 = Machine('LT15', 7000, battery_1, 100)
+    machine_3 = Machine('LT15', 7000, battery_2, 300)
+    machine_4 = Machine('LT15', 7000, battery_2, 100)
 
-def deplete(Machine, Environment):
-    alt = Environment.alt()
-    reduced_w = 50*(alt/100)
-    return -reduced_w
+    temps_1 = [-19.20, -18.20, -22.01, -19, -20, -21, -20.29, -19.20, -18.10]
+    temps_2 = [-10, -12, -9, -10, -12, -9, -10, -12, -9, -10, -12, -9 ]
+    temps_3 = [-10, -40]
 
-## charging function returning the charged wattaged #TODO: Override where you can charge for a set cap, returning time
-def charge(current_wattage, time_to_charge, temperature, alt):
-    charge_rate = 10*temperature/100
-    increased_w = time_to_charge*charge_rate
-    return increased_w
+    environment = Environment('Skøyen - Spikkestad', 110, [temps_2, temps_3])
+    duration = 180
 
-def run_1():
-    m = Machine('LT15',1000,7000,'NMC')
-    b = Battery('NMC', 100,10,500)
-    e = Environment('Spikkestad - Skøyen', 110,15)
-    duration = 720
-
-    #get_stretch(populate(), duration)
-
-    simulate(duration, m,b,e)
+    simulato(duration, [machine_1, machine_1], environment)
+    plt.grid()
     plt.show()
 
-def run_2():
-    m = Machine('LT17',1000,7000,'ABC')
-    b = Battery('Diesel', 100,10,500)
-    e = Environment('Skøyen - Spikkestad', 110,15)
-    duration = 720
-
-    #get_stretch(populate(), duration)
-
-    simulate(duration, m,b,e)
-    plt.show()
-
-run_1()
+scenario_2()
